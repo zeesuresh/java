770ae873e96ccba74d38d513b15cc61a88704e04
zeesuresh <zeesuresh@gmail.com>
2021-03-03 14:51:05 +0530

    Update MainApplication.java

--- src/com/java/training/MainApplication.java
+++ src/com/java/training/MainApplication.java
@@ -3,7 +3,8 @@ package com.java.training;
 public class MainApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Testing GitHub");
+		System.out.println("Testing GitHub1 ");
+		System.out.println("Testing GitHub2 ");
 		System.out.println("Testing GitHub3 ");
 	}
 
